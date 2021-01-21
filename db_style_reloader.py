2abae0003d7f86d29fee9a3b05dc99f330ae162d
Thomas Baumann <thomas.baumann@regiodata-gmbh.de>
2021-01-21 23:32:21 +0100

    added to database menu+toolbar.plugin help added

--- db_style_reloader.py
+++ db_style_reloader.py
@@ -8,18 +8,19 @@
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 # loading by Fahmihorizon from the Noun Project
-# palette by Markus from the Noun Project
-# Grundidee des Reloadens: https://gis.stackexchange.com/a/371589/67477 
+# help by Ruslan Mirsalikhov from the Noun Project
+# palette: https://raw.githubusercontent.com/qgis/QGIS/master/images/themes/default/propertyicons/symbology.svg 
+# based on: https://gis.stackexchange.com/a/371589/67477 
 #---------------------------------------------------------------------
 
 from qgis.core import Qgis, QgsProject
-from qgis.PyQt.QtWidgets import QAction, QMessageBox
+from qgis.PyQt.QtWidgets import QAction, QMessageBox, QMenu
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtCore import QSettings, QLocale, QTranslator, QCoreApplication
 from qgis.PyQt.QtXml import QDomDocument
-from qgis.gui import QgsMessageBar
+from qgis.gui import QgsMessageBar, QgsHelp
 
-import os
+import os, webbrowser
 
 def classFactory(iface):
     return DatabaseStyleLoader(iface)
@@ -30,11 +31,11 @@ class DatabaseStyleLoader:
         self.iface = iface
         self.plugin_dir = os.path.dirname(__file__)
 
-        qgis_locale_id = str(QSettings().value('locale/userLocale'))
-        qgis_locale = QLocale(qgis_locale_id)
-        locale_path = os.path.join(self.plugin_dir, 'i18n')
+        self.qgis_locale_id = str(QSettings().value('locale/userLocale'))
+        self.qgis_locale = QLocale(self.qgis_locale_id)
+        self.locale_path = os.path.join(self.plugin_dir, 'i18n')
         self.translator = QTranslator()
-        self.translator.load(qgis_locale, 'db_style_reloader', '_', locale_path)
+        self.translator.load(self.qgis_locale, 'db_style_reloader', '_', self.locale_path)
         QCoreApplication.installTranslator(self.translator)
 
     def tr(self, message):
@@ -52,13 +53,45 @@ class DatabaseStyleLoader:
         return QCoreApplication.translate('DatabaseStyleLoader', message)
 
     def initGui(self):
-        self.action =  QAction(QIcon(os.path.join(self.plugin_dir,"icon.svg")),"Style Reloader", self.iface.mainWindow())
+        plugin_icon=QIcon(os.path.join(self.plugin_dir,"icons","icon.svg"))
+        help_icon=QIcon(os.path.join(self.plugin_dir,"icons","help.svg"))
+        self.action =  QAction(plugin_icon,self.tr("Reload DB Style"), self.iface.mainWindow())
         self.action.triggered.connect(self.load_style)
-        self.iface.addToolBarIcon(self.action)
+        self.help_action =  QAction(help_icon,self.tr("Help"), self.iface.mainWindow())
+        self.help_action.triggered.connect(self.openHelp)
+        self.iface.addPluginToDatabaseMenu("Style Reloader", self.action)
+        self.iface.addPluginToDatabaseMenu("Style Reloader", self.help_action)
+        if hasattr(self.iface, 'addDatabaseToolBarIcon'):
+            self.iface.addDatabaseToolBarIcon(self.action)
+        else:
+            self.iface.addToolBarIcon(self.action)
+        self.help_menu_main=self.iface.pluginHelpMenu()
+        self.help_menu=QMenu("Style Reloader",self.iface.pluginHelpMenu())
+        self.help_menu.setIcon(plugin_icon)
+        self.help_menu_main.addMenu(self.help_menu)
+        self.help_menu.addAction(self.help_action)
 
     def unload(self):
-        self.iface.removeToolBarIcon(self.action)
+        if hasattr(self.iface, 'removeDatabaseToolBarIcon'):
+            self.iface.removeDatabaseToolBarIcon(self.action)
+        else:
+            self.iface.removeToolBarIcon(self.action)
+        self.iface.removePluginDatabaseMenu("Style Reloader", self.action)
+        self.iface.removePluginDatabaseMenu("Style Reloader", self.help_action)
+        #self.help_menu_main.removeAction(self.help_action)
+        del_object=self.help_menu.menuAction()
+        self.help_menu_main.removeAction(del_object)
         del self.action
+        del self.help_action
+
+    def openHelp(self):
+        user_language = QLocale(QSettings().value(
+            'locale/userLocale')).name()[:2]
+        if user_language == 'de':
+            webbrowser.open(os.path.join(self.plugin_dir,"help","help_{}.html").format(user_language))
+        else:
+            webbrowser.open(os.path.join(self.plugin_dir,"help","help_en.html"))
+        
 
     def load_style(self):
         layers = QgsProject.instance().mapLayers()
@@ -136,7 +169,7 @@ class DatabaseStyleLoader:
         
 
 
-    #schicke generische showMessage Funktion von geotux (gacarrillor)
+    #nice generic showMessage found @geotux (gacarrillor)
     def showMessage(self, message, level=Qgis.Info, target=None, shortmessage=None):
             """
 
